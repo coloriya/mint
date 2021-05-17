4dc2a2b2eaad524461d3f264b62e38c07d24aa72
Ankur Seth <iankurseth@gmail.com>
2021-05-17 06:52:34 +0530

    added empty MintApp class

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const MintApp = require("./mint");
+const mint = new MintApp();
+
+mint.helloWorld();
+
+
---
+++ mint/mintapp.js
@@ -0,0 +1,14 @@
+
+
+
+function MintApp () {
+	//
+}
+
+MintApp.prototype.helloWorld = function () {
+	console.log("MintApp says hello.");
+}
+
+
+
+module.exports = MintApp;
