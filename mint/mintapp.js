b09b03989e1265dff8eae0212335713268e51956
Ankur Seth <iankurseth@gmail.com>
2021-05-18 10:20:17 +0530

    now initing colors and palettes

--- mint/mintapp.js
+++ mint/mintapp.js
@@ -7,16 +7,48 @@ const MintPalette = require("./palette");
 const MintPage = require("./page");
 const MintTemplate = require("./template");
 
+
+
 function MintApp () {
 	this.preferences = JSON.parse(fs.readFileSync("preferences.json"));
 	this.paths = this.preferences.paths;
-	this.colors = [];
-	this.palettes = [];
+	this.setupColors();
+	this.setupPalettes();
 }
+
+MintApp.prototype.setupColors = function () {
+	this.colorsJsonPath = path.join(this.paths.colors, "colors.json");
+	this.colors = [];
+	this.colorObjects = {};
+
+	this.colorsJson = JSON.parse(fs.readFileSync(this.colorsJsonPath));
+	for (let colorJson of this.colorsJson.colors) {
+		let color = new MintColor(this, colorJson);
+		this.colors.push(color);
+		this.colorObjects[color.name] = color;
+	}
+}
+
+MintApp.prototype.setupPalettes = function () {
+	this.palettesJsonPath = path.join(this.paths.colors, "palettes.json");
+	this.palettes = [];
+	this.paletteObjects = {};
+
+	this.palettesJson = JSON.parse(fs.readFileSync(this.palettesJsonPath));
+	for (let paletteJson of this.palettesJson.palettes) {
+		let palette = new MintPalette(this, paletteJson);
+		this.palettes.push(palette);
+		this.paletteObjects[palette.name] = palette;
+	}
+}
+
+
 
 MintApp.prototype.helloWorld = function () {
 	// console.log("MintApp says hello.");
-	console.log(this.paths);
+	console.log(`MintApp setup complete:`);
+	console.log(`\t* ${this.colors.length} colors`);
+	console.log(`\t* ${this.palettes.length} palettes`);
 }
 
 
--- mint/palette.js
+++ mint/palette.js
@@ -1,8 +1,28 @@
 
 
 
-function MintPalette () {
-	//
+function MintPalette (app, json) {
+	this.app = app;
+	this.json = json;
+	this.name = json.name;
+	this.title = json.title;
+}
+
+
+
+MintPalette.prototype.getTitle = function () {
+	return this.title;
+}
+
+MintPalette.prototype.getName = function () {
+	return this.name;
+}
+
+MintPalette.prototype.getDescription = function () {
+	if (this.json.description) {
+		return this.json.description;
+	}
+	return "No description for palette!";
 }
 
 
