c67b15f7484622fd24aab66548d33117405b710f
Ankur Seth <iankurseth@gmail.com>
2021-05-18 18:40:13 +0530

    list commands now start with ls

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const command = args[2] ? args[2].toLowerCase() : null;
 const arg = args[3] ? args[3] : null;
 
 switch (command) {
-	case "lc":
+	case "lsc":
 	case "listcolors":
 		mint.listColors();
 		break;
-	case "lp":
+	case "lsp":
 	case "listpalettes":
 		mint.listPalettes();
 		break;
-	case "lt":
+	case "lst":
 	case "listtemplates":
 		mint.listTemplates();
 		break;
-	case "ld":
+	case "lsd":
 	case "listdesigns":
 		mint.listDesigns();
 		break;
