712e1457dee77c23b3112a08babff511ab85ec64
Ankur Seth <iankurseth@gmail.com>
2021-05-19 10:19:19 +0530

    more compact switches

--- app.js
+++ app.js
@@ -11,61 +11,32 @@ switch (command) {
 	case "ls":
 	case "list":
 		switch (arg) {
-			case "colors":
-				mint.listColors();
-				break;
-			case "palettes":
-				mint.listPalettes();
-				break;
-			case "templates":
-				mint.listTemplates();
-				break;
-			case "designs":
-				mint.listDesigns();
-				break;
-			case "pages":
-				mint.listPages();
-				break;
-			default:
-				console.log("Default list.");
+			case "colors": mint.listColors(); break;
+			case "palettes": mint.listPalettes(); break;
+			case "templates": mint.listTemplates(); break;
+			case "designs": mint.listDesigns(); break;
+			case "pages": mint.listPages(); break;
+			default: console.log("Default list.");
 		}
 		break;
 
 	case "render":
 		switch (arg) {
-			case "colors":
-				mint.renderColors();
-				break;
-			case "palettes":
-				mint.renderPalettes();
-				break;
-			case "designs":
-				mint.renderDesigns();
-				break;
-			case "pages":
-				mint.renderPages();
-				break;
-			default:
-				console.log("Default render.");
+			case "colors": mint.renderColors(); break;
+			case "palettes": mint.renderPalettes(); break;
+			case "designs": mint.renderDesigns(); break;
+			case "pages": mint.renderPages(); break;
+			default: console.log("Default render.");
 		}
 		break;
 
 	case "detail":
 		switch (arg) {
-			case "color":
-				mint.colorDetails(subarg);
-				break;
-			case "palette":
-				mint.paletteDetails(subarg);
-				break;
-			case "template":
-				mint.templateDetails(subarg);
-				break;
-			case "design":
-				mint.designDetails(subarg);
-				break;
-			default:
-				//
+			case "color": mint.colorDetails(subarg); break;
+			case "palette": mint.paletteDetails(subarg); break;
+			case "template": mint.templateDetails(subarg); break;
+			case "design": mint.designDetails(subarg); break;
+			default: console.log("Default detail.");
 		}
 		break;
 
